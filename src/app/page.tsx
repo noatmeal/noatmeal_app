6cb6b68255de5e56efc453693eb8c4f739039ef3
Noatmeal <noatmeal94@gmail.com>
2025-03-09 09:12:30 -0500

    Some final styling before moving towards getting this online.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { ToggleLightAndDark } from "@/components/toggle-light-and-dark";
 
 export default function Home() {
   return (
-    <>
-      <div>And so it begins...</div>
+    <div className="h-screen grid place-items-center font-roboto">
+      <span>And so it begins...</span>
       <ToggleLightAndDark></ToggleLightAndDark>
-    </>
+    </div>
   );
 }
