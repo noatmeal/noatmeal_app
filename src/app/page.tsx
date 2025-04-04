a148006ea0cc9577df4ba2bceaa6f89d52cfae48
Noatmeal <noatmeal94@gmail.com>
2025-04-04 07:23:59 -0500

    Adding the Right text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,26 @@ export default function Home() {
         <ModeToggle />
       </div>
       
-      <h1 className="text-4xl font-bold">Welcome to My Site</h1>
+      <h2>Greetings!</h2>
       
-      <p className="text-lg">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris. 
-        Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus 
-        rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna 
-        non est bibendum non venenatis nisl tempor.
+      <p>
+        I'm Noatmeal and this is my blog. My goal is to mostly write about 
+        computer science, programming, and mathematics, but life has a way of 
+        being distracting and so I'm bound to write about other things here and 
+        there. 
       </p>
-      
-      <p className="text-lg">
-        Suspendisse potenti. Sed egestas, ante et vulputate volutpat, eros pede semper 
-        est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, 
-        commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor.
+     
+      <h2>Contact</h2> 
+      <p>
+         Send an email to Noatmeal 94 (at) gmail dot com
+      </p>
+       
+      <p>
+        I mostly use social media as a way to advertise what I'm doing here. So 
+        I likely won't respond to any social media accounts you see by me in the 
+        wild and would prefer that you just email me. However, if you're aware 
+        of any interesting discussions that you'd like me to see or want me to 
+        weigh in on, let me know and I'll take a look! 
       </p>
     </div>
   );
