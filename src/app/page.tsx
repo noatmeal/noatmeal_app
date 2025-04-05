68d9f81dca51d2f73d8dd9fd9dda16c1fa618178
Noatmeal <noatmeal94@gmail.com>
2025-04-05 06:14:13 -0500

    Header and better yaml use.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
-import { Navigation } from "@/components/navigation";
-import fs from "fs";
-import path from "path";
-import yaml from "yaml";
+import { Header } from "@/components/header";
+import { getYamlContent } from '@/lib/yaml-loader'
 
-const contentPath = path.join(process.cwd(), "src/content/home.yaml");
-const content = yaml.parse(fs.readFileSync(contentPath, "utf8"));
 
-export default function Home() {
+export default async function Home() {
+  const content = await getYamlContent("src/content/home.yaml");
+
   return (
-    <div>
-      <h1>{content.title}</h1>
-      <Navigation />
+    <>
+      <Header />
+      <div>
+        <h1>{content.greetings.title}</h1>
+        <p>{content.greetings.content}</p>
 
-      <h1>{content.greetings.title}</h1>
-      <p>{content.greetings.content}</p>
-
-      <h1>{content.contact.title}</h1>
-      <p>Send an email to {content.contact.email}</p>
-      <p>{content.contact.content}</p>
-    </div>
+        <h1>{content.contact.title}</h1>
+        <p>{content.contact.email}</p>
+        <p>{content.contact.noSocialMedia}</p>
+      </div>
+    </>
   );
 }
