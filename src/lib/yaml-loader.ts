b2771c6e469044f07d61d0280ba3a77be4533a32
Noatmeal <noatmeal94@gmail.com>
2025-04-05 06:23:03 -0500

    Fixing typescript error

--- src/lib/yaml-loader.ts
+++ src/lib/yaml-loader.ts
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 import path from "path";
 import yaml from "yaml";
 
-export async function getYamlContent(filename) {
+export async function getYamlContent(filename: string) {
   const filePath = path.join(process.cwd(), filename);
   const fileContents = await fs.readFile(filePath, "utf8");
   return yaml.parse(fileContents);
