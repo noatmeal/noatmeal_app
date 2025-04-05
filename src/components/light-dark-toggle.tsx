942f9a2d7ff7ed4802242ef187f017fc9ed0af1e
Noatmeal <noatmeal94@gmail.com>
2025-04-05 05:10:29 -0500

    Adding prettier

--- src/components/light-dark-toggle.tsx
+++ src/components/light-dark-toggle.tsx
@@ -1,43 +1,50 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="text-foreground bg-background border-foreground cursor-pointer">
+        <Button
+          variant="outline"
+          size="icon"
+          className="text-foreground bg-background border-foreground cursor-pointer"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="bg-background/80 backdrop-blur-3xl">
-        <DropdownMenuItem 
+      <DropdownMenuContent
+        align="end"
+        className="bg-background/80 backdrop-blur-3xl"
+      >
+        <DropdownMenuItem
           onClick={() => setTheme("light")}
           className="hover:underline cursor-pointer"
         >
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem 
+        <DropdownMenuItem
           onClick={() => setTheme("dark")}
           className="hover:underline cursor-pointer"
         >
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem 
+        <DropdownMenuItem
           onClick={() => setTheme("system")}
           className="hover:underline cursor-pointer"
         >
@@ -45,6 +52,5 @@ export function ModeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
-
