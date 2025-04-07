dd9ebe53052fee97b8dc2050439a17f3eed7bd85
Noatmeal <noatmeal94@gmail.com>
2025-04-07 06:54:31 -0500

    First blog post, yay!

--- src/components/blog-posts/the-near-future-2025-03-20.tsx
+++ src/components/blog-posts/the-near-future-2025-03-20.tsx
@@ -2,92 +2,75 @@ import React from "react";
 
 import Link from "next/link";
 
-interface ContentItem {
-  content: string;
-}
-
-interface LinkItem {
-  content: string;
-  link: string;
-}
-
-interface FourthWantContent {
-  sentence: {
-    firstPart: string;
-    secondPart: LinkItem;
-    finalPart: string;
-  };
-}
-
-interface ListOfWants {
-  firstWant: ContentItem;
-  secondWant: ContentItem;
-  thirdWant: ContentItem;
-  fourthWant: { content: FourthWantContent };
-}
-
-interface TheNearFutureContent {
-  title: string;
-  date: string;
-  firstParagraph: ContentItem;
-  listOfWants: ListOfWants;
-  secondParagraph: ContentItem & { links?: { [key: string]: string } };
-  thirdParagraph: ContentItem;
-}
-
-interface TheNearFuture20250320Props {
-  content: TheNearFutureContent;
-}
-
-export function TheNearFuture20250320({ content }: TheNearFuture20250320Props) {
-  const {
-    title,
-    firstParagraph,
-    listOfWants,
-    secondParagraph,
-    thirdParagraph,
-  } = content;
+export function TheNearFuture20250320({ content }) {
+  const { paragraphs, title } = content;
+  console.log(paragraphs[3])
   return (
     <>
       <h1>{title}</h1>
 
-      <p>{firstParagraph.content}</p>
+      <p>{paragraphs[0].sections[0]}</p>
 
       <ul className="list-none pl-0 my-4">
         <li
           key="firstWant"
-          className="flex items-baseline mb-2 before:content-['->'] before:mr-2 before:flex-shrink-0"
+          className="flex items-baseline mb-2 
+                     before:content-['->'] before:mr-2 before:flex-shrink-0"
         >
-          {listOfWants.firstWant.content}
+          {paragraphs[0].sections[1].sentences[0]}
         </li>
         <li
           key="secondWant"
-          className="flex items-baseline mb-2 before:content-['->'] before:mr-2 before:flex-shrink-0"
+          className="flex items-baseline mb-2 
+                     before:content-['->'] before:mr-2 before:flex-shrink-0"
         >
-          {listOfWants.secondWant.content}
+          {paragraphs[0].sections[1].sentences[1]}
         </li>
         <li
           key="thirdWant"
-          className="flex items-baseline mb-2 before:content-['->'] before:mr-2 before:flex-shrink-0"
+          className="flex items-baseline mb-2 
+                     before:content-['->'] before:mr-2 before:flex-shrink-0"
         >
-          {listOfWants.thirdWant.content}
+          {paragraphs[0].sections[1].sentences[2]}
         </li>
         <li
           key="fourthWant"
-          className="flex items-baseline mb-2 before:content-['->'] before:mr-2 before:flex-shrink-0"
+          className="flex items-baseline mb-2 
+                     before:content-['->'] before:mr-2 before:flex-shrink-0"
         >
-          {listOfWants.fourthWant.content.sentence.firstPart}
+          {paragraphs[0].sections[1].sentences[3].parts[0]}
           <span>&nbsp;</span>
-          <Link href={listOfWants.fourthWant.content.sentence.secondPart.link}>
-            {listOfWants.fourthWant.content.sentence.secondPart.content}
+          <Link href={paragraphs[0].sections[1].sentences[3].parts[1].link}>
+            {paragraphs[0].sections[1].sentences[3].parts[1].text}
           </Link>
-          {listOfWants.fourthWant.content.sentence.finalPart}
+          {paragraphs[0].sections[1].sentences[3].parts[2]}
         </li>
       </ul>
 
-      <p>{secondParagraph.content}</p>
+      <p>
+        {paragraphs[1].sections[0]}
+        {" "}
+        {paragraphs[1].sections[1].sentences[0].parts[0]}
+        {" "}
+        <Link href={paragraphs[1].sections[1].sentences[0].parts[1].link}>
+          {paragraphs[1].sections[1].sentences[0].parts[1].text}
+        </Link>
+        {" "}
+        {paragraphs[1].sections[1].sentences[0].parts[2]}
+      </p>
 
-      <p>{thirdParagraph.content}</p>
+
+      <p>
+        {paragraphs[2].sections[0].sentences[0].parts[0]}
+        <Link href={paragraphs[2].sections[0].sentences[0].parts[1].link}>
+          {paragraphs[2].sections[0].sentences[0].parts[1].text}
+        </Link>
+        {paragraphs[2].sections[0].sentences[0].parts[2]}
+        {paragraphs[2].sections[1]}
+      </p>
+
+      <p>{paragraphs[3]}</p>
+
     </>
   );
 }
