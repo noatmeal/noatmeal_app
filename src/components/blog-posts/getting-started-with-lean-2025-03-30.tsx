ce353b8786ed2a58a27d152b0ce930ed44e8f73c
Noatmeal <noatmeal94@gmail.com>
2025-04-08 06:28:11 -0500

    Almost there.

---
+++ src/components/blog-posts/getting-started-with-lean-2025-03-30.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import Link from "next/link";
+
+export function GettingStartedWithLean20250330({ content }) {
+  const { chapters, title } = content;
+  console.log(chapters[1].paragraphs[1]);
+
+  return (
+    <>
+      <h1>{title}</h1>
+
+      <p>
+        {chapters[0].paragraphs[0].sections[0].sentences[0].parts[0]}{" "}
+        <Link
+          href={
+            chapters[0].paragraphs[0].sections[0].sentences[0].parts[1].link
+          }
+        >
+          {chapters[0].paragraphs[0].sections[0].sentences[0].parts[1].text}
+        </Link>{" "}
+        {chapters[0].paragraphs[0].sections[0].sentences[0].parts[2]}{" "}
+        {chapters[0].paragraphs[0].sections[1]}
+      </p>
+
+      <p>
+        {chapters[0].paragraphs[1].sections[0].sentences[0].parts[0]}{" "}
+        <Link
+          href={
+            chapters[0].paragraphs[1].sections[0].sentences[0].parts[1].link
+          }
+        >
+          {chapters[0].paragraphs[1].sections[0].sentences[0].parts[1].text}
+        </Link>{" "}
+        {chapters[0].paragraphs[1].sections[0].sentences[0].parts[2]}{" "}
+        <Link
+          href={
+            chapters[0].paragraphs[1].sections[0].sentences[0].parts[3].link
+          }
+        >
+          {chapters[0].paragraphs[1].sections[0].sentences[0].parts[3].text}
+        </Link>
+        {chapters[0].paragraphs[1].sections[0].sentences[0].parts[4]}{" "}
+        {chapters[0].paragraphs[1].sections[0].sentences[1]}{" "}
+        {chapters[0].paragraphs[1].sections[0].sentences[2].parts[0]}{" "}
+        <Link
+          href={
+            chapters[0].paragraphs[1].sections[0].sentences[2].parts[1].link
+          }
+        >
+          {chapters[0].paragraphs[1].sections[0].sentences[2].parts[1].text}
+        </Link>{" "}
+        {chapters[0].paragraphs[1].sections[0].sentences[2].parts[2]}{" "}
+        <Link
+          href={
+            chapters[0].paragraphs[1].sections[0].sentences[2].parts[3].link
+          }
+        >
+          {chapters[0].paragraphs[1].sections[0].sentences[2].parts[3].text}
+        </Link>{" "}
+        {chapters[0].paragraphs[1].sections[0].sentences[2].parts[4]}{" "}
+        {chapters[0].paragraphs[1].sections[0].sentences[3]}
+      </p>
+
+      <h2>{chapters[1].title}</h2>
+
+      <p>{chapters[1].paragraphs[0]}</p>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>{chapters[1].paragraphs[1].file}</CardTitle>
+          <CardDescription>
+            {chapters[1].paragraphs[1].repository}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>{chapters[1].paragraphs[1].lines.code[0]}</div>
+          <div>{chapters[1].paragraphs[1].lines.code[1]}</div>
+        </CardContent>
+        <CardFooter>
+          <p>{chapters[1].paragraphs[1].lines.output[0]}</p>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
